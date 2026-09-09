237ea7ef05139cf59de475bc8125bec39d3ad4e4
JOSE GRACIANO <thegoldenblood30@gmail.com>
2026-09-08 21:35:23 -0500

    Delete main directory

--- main/java/com/llegaya/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.llegaya.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.llegaya.model.User;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
